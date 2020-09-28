dc8cb52dc4136e4cfa4e6ac983b583835551b5ec
Liang-Wu <stevenwu12@outlook.com>
2020-09-27 23:16:50 -0400

    update 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,7 @@ namespace API
             services.AddDbContext<DataContext>(options=>{
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnectionString"));
             });
+            services.AddCors();
             // services.AddSwaggerGen(c =>
             // {
             //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -52,6 +53,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(Policy => Policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
